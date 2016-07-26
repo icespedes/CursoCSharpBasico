e512cae6b8a20d19531a515de9801363431a761f
Ignacio Céspedes <b.ignacio.cespedes@gmail.com>
2016-07-26 01:02:49 -0400

    Herencia, Accesadores, Abstract, Virtual, Override

---
+++ Institucion/Models/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institucion.Models
+{
+    public abstract class Persona
+    {
+        public static int ContadorPersonas = 0;
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public short Edad { get; set; }
+        public string Teléfono { get; set; }
+        protected int Inasistencias { get; set; }
+        public virtual string NombreCompleto
+        {
+            get
+            {
+                return $"{Nombre} {Apellido}"; //String Literals $  || Otra forma es return string.format("{0} {1}", Nombre, Apellido).
+            }
+        }
+
+        public Persona()
+        {
+            ContadorPersonas++; //es igual que escribir ContadorPersonas = ContadorPersonas + 1 || ContadorPersonas += 1
+        }
+
+        public abstract string ConstruirResumen();
+    }
+}
---
+++ Institucion/Program.cs
@@ -0,0 +1,59 @@
+﻿using Institucion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GESTION DE INSTITUCION");
+
+            Persona[] lista = new Persona[3];
+            lista[0] = new Alumno("Ignacio", "Cespedes")
+            {
+                Id = 1,
+                Edad = 26,
+                Teléfono = "71364790",
+                Email = "b.ignacio.cespedes@gmail.com"
+            };
+
+            lista[1] = new Profesor()
+            {
+                Id = 2,
+                Nombre = "Ignacio",
+                Apellido = "Cespedes",
+                Edad = 26,
+                Teléfono = "71364790",
+                Catedra = "Programación"
+            };
+
+            lista[2] = new Profesor()
+            {
+                Id = 2,
+                Nombre = "William",
+                Apellido = "Torvalds",
+                Edad = 25,
+                Teléfono = "71364911",
+                Catedra = "Algebra"
+            };
+
+            Console.WriteLine(Persona.ContadorPersonas);
+            Console.WriteLine("Resumenes");
+
+            //Para cada persona p en la lista llama al metodo construirResumen
+            foreach (Persona p in lista)
+            {
+                Console.WriteLine($"Tipo {p.GetType()}");
+                Console.WriteLine($"{p.ConstruirResumen()}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
